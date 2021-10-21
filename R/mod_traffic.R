6db6e25e89da70974762cefa4daa4eae7ea8d9d3
Ryan Morton <mortonanalytics@gmail.com>
2021-10-21 14:55:04 -0600

    update

--- R/mod_traffic.R
+++ R/mod_traffic.R
@@ -4,7 +4,15 @@ mod_traffic_ui <- function(id){
     fluidRow(
       column(
         12
-        ,leafletOutput(ns("traffic"), width = "100%")
+        ,dropdown(
+          mod_traffic_uc_ui(ns("uc"))
+        )
+      )
+    )
+    ,fluidRow(
+      column(
+        12
+        ,leafletOutput(ns("traffic"), width = "100%", height = "800px")
       )
     )
   )
