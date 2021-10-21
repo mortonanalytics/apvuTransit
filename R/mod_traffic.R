b5cf5820fdbe3b4350ed159d80529843c9a5eb57
Ryan Morton <mortonanalytics@gmail.com>
2021-10-21 15:02:40 -0600

    eod layout modify commit

--- R/mod_traffic.R
+++ R/mod_traffic.R
@@ -2,16 +2,14 @@ mod_traffic_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-      column(
-        12
-        ,dropdown(
-          mod_traffic_uc_ui(ns("uc"))
-        )
-      )
-    )
-    ,fluidRow(
-      column(
-        12
+       column(
+         2
+         ,dropdown(
+           mod_traffic_uc_ui(ns("uc"))
+         )
+       )
+      ,column(
+         8
         ,leafletOutput(ns("traffic"), width = "100%", height = "800px")
       )
     )
